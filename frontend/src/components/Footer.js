e73d81baddc01c94d7803c2a9735ee43adc77459
Anton Friberg <antonfr@axis.com>
2018-10-22 10:27:37 +0200

    Remove ops bits to prepare open source
    Removes residues from pruning the git history in preparation of the
    open source efforts.
    
    Change-Id: I930f56d56c12478cc87472084f8a0153c48bdaf8

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,24 +1,13 @@
 import React from "react";
-import logo from "../assets/images/logo_small.png";
 
 function Footer() {
   return (
     <footer>
       <div className="container footer">
-        <div className="row text-center">
-          <a href="https://www.example.com/">
-            <img id="Company-Logo" src={logo} alt="Company Logo" />
-          </a>
-        </div>
 
         <div className="row text-center">
-          <a href="https://docs.example.com">
-            
-          </a>
-          <br />
-          <a href="mailto:support@example.com?subject=go-service support">
-            Contact: support@example.com
-          </a>
+          go/ URL redirector | Source available on&#160;
+          <a href="https://github.com">GitHub</a>
         </div>
       </div>
     </footer>
